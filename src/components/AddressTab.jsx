83221754e6792c9e011baca6f6f669a1ad7df9ea
stephen <lourdustephen95@gmail.com>
2022-05-25 18:50:25 +0530

    docto_edit_view

--- src/components/AddressTab.jsx
+++ src/components/AddressTab.jsx
@@ -14,15 +14,15 @@ import { useNavigate } from "react-router-dom";
 
 function AddressTab() {  
   const history = useNavigate();
-  const [description, setDescription] = useState(true);
-  const [reviews, setReviews] = useState(false);
-  const descriptionHandler = () => {
-      setDescription(true);
-      setReviews(false);
+  const [homeBtn, setHomeBtn] = useState(true);
+  const [workBtn, setWorkBtn] = useState(false);
+  const homeBtnHandler = () => {
+    setHomeBtn(true);
+    setWorkBtn(false);
   };
-  const reviewsHandler = () => {
-    setDescription(false);
-    setReviews(true);
+  const workBtnHandler = () => {
+    setHomeBtn(false);
+    setWorkBtn(true);
   }
 
   // home address input field
@@ -38,27 +38,25 @@ function AddressTab() {
       setWorkAddressAddField(tabs => {
             return [ ...tabs, "end"];});
       };  
-     
   
   return ( 
-    
-  
    <>
+   
     <div className='home-tab'>
     <Row style={{justifyContent:"center"}}>
-     <button className='profile-text' onClick={descriptionHandler} > Home</button>
+     <button className='profile-text' onClick={homeBtnHandler} > Home</button>
      <Divider type='vertical' style={{backgroundColor:"black",width:"2px",height:'18px',marginTop:'5px'}}/>
-     <button className='profile-text' key="Work" onClick={reviewsHandler}> Work</button>
+     <button className='profile-text' key="Work" onClick={workBtnHandler}> Work</button>
    </Row>
-   {description && (
+   {homeBtn && (
     <div >
      <form>  
          <p style={{color:"#702cc7"}}>Address<img alt="" src={ IconAddRound} onClick={addHomeAddressInput} style={{marginLeft:"264px", marginRight:"-4px"}}/> </p>  
-           <input  className='form-input-field' placeholder='Address 1'type="text" />
-           <input  className='form-input-field' placeholder='Address 2' type="text" />
-           <input  className='form-input-field' placeholder='City' type="text" />
-           <input  className='form-input-field' placeholder='District' type="text" />
-           <input  className='form-input-field' placeholder='State' type="text" />
+           <input disabled={isView} className='form-input-field' placeholder='Address 1'type="text" />
+           <input disabled={isView} className='form-input-field' placeholder='Address 2' type="text" />
+           <input disabled={isView} className='form-input-field' placeholder='City' type="text" />
+           <input disabled={isView} className='form-input-field' placeholder='District' type="text" />
+           <input disabled={isView} className='form-input-field' placeholder='State' type="text" />
            <div>
                 {homeAddressAddField.map((item, i) => {
                   return (
@@ -74,23 +72,23 @@ function AddressTab() {
      </form>
      <form>  
          <p style={{color:"#702cc7",marginLeft:"-279px"}}>Country </p>  
-           <input  className='form-input-field' placeholder='Country'type="text" />
-           <input  className='form-input-field' placeholder='Mobile number' type="number"  />     
+           <input disabled={isView}  className='form-input-field' placeholder='Country'type="text" />
+           <input disabled={isView} className='form-input-field' placeholder='Mobile number' type="number"  />     
      </form>
      <Button className='btn save-btn'>Save  <img src={ IconSave } alt="" style={{marginLeft:"80px"}} /></Button>
      <Button className='btn next-btn' onClick={() => history("/userProfilestore")}>Next <img src={ IconNextArrow } alt=""  style={{marginLeft:"80px"}}/></Button>
      </div>
      )} 
-     {reviews && ( 
+     {workBtn && ( 
      <div >
       
      <form>  
           <p style={{color:"#702cc7"}}>Address < img alt='' src={ IconAddRound} onClick={addWorkAddressInput} style={{marginLeft:"264px", marginRight:"-4px"}}/> </p>  
-           <input  className='form-input-field' placeholder='Address 1'type="text" />
-           <input  className='form-input-field' placeholder='Address 2' type="text" />
-           <input  className='form-input-field' placeholder='City' type="text" />
-           <input  className='form-input-field' placeholder='District' type="text" />
-           <input  className='form-input-field' placeholder='State' type="text" />
+           <input disabled={isView} className='form-input-field' placeholder='Address 1'type="text" />
+           <input disabled={isView} className='form-input-field' placeholder='Address 2' type="text" />
+           <input disabled={isView} className='form-input-field' placeholder='City' type="text" />
+           <input disabled={isView} className='form-input-field' placeholder='District' type="text" />
+           <input disabled={isView} className='form-input-field' placeholder='State' type="text" />
            <div>
                 {workAddressAddField.map((item, i) => {
                   return (
@@ -106,8 +104,8 @@ function AddressTab() {
      </form>
      <form>  
          <p style={{color:"#702cc7",marginLeft:"-279px"}}>Country </p>  
-           <input  className='form-input-field' placeholder='Country'type="text" />
-           <input  className='form-input-field' placeholder='Mobile number' type="number" />     
+           <input disabled={isView}  className='form-input-field' placeholder='Country'type="text" />
+           <input disabled={isView}   className='form-input-field' placeholder='Mobile number' type="number" />     
      </form>
      <Button className='btn save-btn'>Save  <img src={ IconSave } alt="" style={{marginLeft:"80px"}} /></Button>
      <Button className='btn next-btn' onClick={() => history("/userProfilestore")}>Next <img src={ IconNextArrow } alt="" style={{marginLeft:"80px"}}/></Button>
--- src/components/TabForm.jsx
+++ src/components/TabForm.jsx
@@ -10,12 +10,12 @@ import React, {useState} from 'react';
 // import AddressTab from './AddressTab';
 import Header from './Header';
 
-import {Button,  Tabs } from 'antd';
+import {Button,  Tabs, Divider,Row } from 'antd';
 import IconSave from "../assets/icon/IconSave.svg";
 import IconNextArrow from "../assets/icon/IconNextArrow.svg";
 import IconAddRound from "../assets/logo/IconAddRound.png";
-import AddressTab from './AddressTab';
-// import { useNavigate } from "react-router-dom";
+
+import { useNavigate } from "react-router-dom";
 const { TabPane } = Tabs;
 
 
@@ -24,7 +24,7 @@ const { TabPane } = Tabs;
 
 // eslint-disable-next-line
 function TabForm() {
-  // const history = useNavigate();
+ const history = useNavigate();
 
   const [currentTab, setCurrentTab] = useState("Profile");
   function callback(key) {
@@ -65,45 +65,82 @@ function TabForm() {
        setprofessionAddField(tabs => {
          return [ ...tabs, "end"];});
      };
-  
+    //address home & work button 
+     const [homeBtn, setHomeBtn] = useState(true);
+     const [workBtn, setWorkBtn] = useState(false);
+     const homeBtnHandler = () => {
+       setHomeBtn(true);
+       setWorkBtn(false);
+     };
+     const workBtnHandler = () => {
+       setHomeBtn(false);
+       setWorkBtn(true);
+     }
+   
+     // home address input field
+     const homeAddressInput = [];
+     const [homeAddressAddField, setHomeAddressAddField] = useState(homeAddressInput);
+     const addHomeAddressInput = () => {
+       setHomeAddressAddField(tabs => {
+           return [ ...tabs, "end"];});
+         };  
+     const workAddressInput = [];
+     const [workAddressAddField, setWorkAddressAddField] = useState(workAddressInput);
+     const addWorkAddressInput = () => {
+         setWorkAddressAddField(tabs => {
+               return [ ...tabs, "end"];});
+         };  
+     
+     //view-edit mode
+    const [isView,setIsView] = useState(true)
+    const handleClick =() => {
+      setIsView(true)
+        
+    }
+    const handleClick2 =() => {
+      setIsView(false)
+    }
   return (
     <>
+     <div style={{textAlign:"center"}}>
+     <button style={{width:"60px",borderRadius:"29px",marginRight:'5px',backgroundColor:"cyan",borderWidth:"2px"}} onClick={handleClick}>View</button>
+     <button style={{width:"60px",borderRadius:"29px",marginRight:'5px',backgroundColor:"cornsilk",borderWidth:"2px"}} onClick={handleClick2}>Edit</button>
+    </div>
+    
     <Header />
    
     <div className='tab-align' >
-    <div className='tab-bg' >
-      <Tabs onChange={callback}  type="card" 
-      activeKey ={currentTab}
-     
-    > 
+      <div className='tab-bg' >
+        <Tabs onChange={callback}  type="card" 
+         activeKey ={currentTab}> 
       
         <TabPane onTabClick={callbackTabClicked} tab="Profile" key="Profile"  >
         <div className='home-tab'>
-      <h1 className='profile-text'> Profile Details</h1>
-        <form>  
-            <input  className='form-input-field' placeholder='First Name'type="text" />
-            <input  className='form-input-field' placeholder='Last Name' type="text" />
-            <input  className='form-input-field' placeholder='Date of birth' type="text" />
-            <input  className='form-input-field' placeholder='Doctor license number' type="number" />
-            <input  className='form-input-field' placeholder='Phone Number' type="number" />
-            <input  className='form-input-field' placeholder='Email' type="text" />
-        </form>
+         <h1 className='profile-text'> Profile Details</h1>
+          <form>  
+            <input disabled={isView}  className='form-input-field' placeholder='First Name'type="text" />
+            <input disabled={isView}  className='form-input-field' placeholder='Last Name' type="text" />
+            <input disabled={isView}  className='form-input-field' placeholder='Date of birth' type="date" style={{textTransform:"capitalize"}} />
+            <input disabled={isView}  className='form-input-field' placeholder='Doctor license number' type="number" />
+            <input disabled={isView}  className='form-input-field' placeholder='Phone Number' type="number" />
+            <input disabled={isView}  className='form-input-field' placeholder='Email' type="email" />
+         </form>
         <Button className='btn save-btn'>Save  <img src={ IconSave } alt="" style={{marginLeft:"80px"}} /></Button>
         <Button className='btn next-btn' onClick={handleSelect}>Next <img src={ IconNextArrow } alt="" style={{marginLeft:"80px"}}/></Button>
           </div>
-        </TabPane>
+          </TabPane>
 
         
        
         
         <TabPane onTabClick={callbackTabClicked} tab="Education" key="Education">
         <div className='home-tab'>
-        <h1 className='profile-text'> Education Details</h1>
+         <h1 className='profile-text'> Education Details</h1>
           <form>  
               <p style={{color:"#702cc7"}}>Add education details form  <img alt="" src={ IconAddRound} onClick={addEduInput} style={{marginLeft:"146px"}}/> </p>  
-              <input  className='form-input-field' placeholder='Education deatils 1'type="text" />
-              <input  className='form-input-field' placeholder='Education deatils 2' type="text" />
-              <input  className='form-input-field' placeholder='Education deatils 3' type="text" />
+              <input disabled={isView}  className='form-input-field' placeholder='Education deatils 1'type="text" />
+              <input disabled={isView}  className='form-input-field' placeholder='Education deatils 2' type="text" />
+              <input disabled={isView}  className='form-input-field' placeholder='Education deatils 3' type="text" />
               <div>
                 {educationAddField.map((item, i) => {
                   return (
@@ -124,17 +161,84 @@ function TabForm() {
           </TabPane> 
 
         <TabPane onTabClick={callbackTabClicked} tab="Work History" key="Work History">
+          <div className='home-tab'>
+           <h1 className='profile-text'>Work History Details </h1>
+            <form>  
+                <p style={{color:"#702cc7" }}>Add work history Details form  <img alt="" src={ IconAddRound} onClick={addProfessInput} style={{marginLeft:"146px"}}/> </p>  
+                <input disabled={isView}  className='form-input-field' placeholder='Clinic Name'type="text" />
+                <input disabled={isView}  className='form-input-field' placeholder='Clinic address' type="text" />
+                <input disabled={isView}  className='form-input-field' placeholder='Clinic phone number' type="number"  />
+                <input disabled={isView}  className='form-input-field' placeholder='Notes' type="text" />
+                  {/* input fields */}
+                  <div>
+                  {professionAddField.map((item, i) => {
+                    return (
+                      <input
+                        // value={item.value}
+                        // id={i}
+                        className="form-input-field"
+                        // type={item.type}  
+                        />
+                    );
+                  })}
+                </div>
+            </form>
+            <Button className='btn save-btn'>Save  <img src={ IconSave } alt="" style={{marginLeft:"80px"}} /></Button>
+            <Button className='btn next-btn' onClick={handleSelect2}>Next <img src={ IconNextArrow } alt="" style={{marginLeft:"80px"}}/></Button>
+          </div>
+
+        </TabPane>
+      
+        <TabPane  onTabClick={callbackTabClicked} tab="Address" key="Address">
         <div className='home-tab'>
-        <h1 className='profile-text'>Work History Details </h1>
-          <form>  
-              <p style={{color:"#702cc7" }}>Add work history Details form  <img alt="" src={ IconAddRound} onClick={addProfessInput} style={{marginLeft:"146px"}}/> </p>  
-              <input  className='form-input-field' placeholder='Clinic Name'type="text" />
-              <input  className='form-input-field' placeholder='Clinic address' type="text" />
-              <input  className='form-input-field' placeholder='Clinic phone number' type="number"  />
-              <input  className='form-input-field' placeholder='Notes' type="text" />
-                {/* input fields */}
-                <div>
-                {professionAddField.map((item, i) => {
+           <Row style={{justifyContent:"center"}}>
+     <button className='profile-text' onClick={homeBtnHandler} > Home</button>
+     <Divider type='vertical' style={{backgroundColor:"black",width:"2px",height:'18px',marginTop:'5px'}}/>
+     <button className='profile-text' key="Work" onClick={workBtnHandler}> Work</button>
+            </Row>
+   {homeBtn && (
+    <div >
+     <form>  
+         <p style={{color:"#702cc7"}}>Address<img alt="" src={ IconAddRound} onClick={addHomeAddressInput} style={{marginLeft:"264px", marginRight:"-4px"}}/> </p>  
+           <input disabled={isView} className='form-input-field' placeholder='Address 1'type="text" />
+           <input disabled={isView} className='form-input-field' placeholder='Address 2' type="text" />
+           <input disabled={isView} className='form-input-field' placeholder='City' type="text" />
+           <input disabled={isView} className='form-input-field' placeholder='District' type="text" />
+           <input disabled={isView} className='form-input-field' placeholder='State' type="text" />
+           <div>
+                {homeAddressAddField.map((item, i) => {
+                  return (
+                    <input
+                      // value={item.value}
+                      // id={i}
+                      className="form-input-field"
+                      // type={item.type}  
+                       />
+                  );
+                })}
+           </div>
+     </form>
+     <form>  
+         <p style={{color:"#702cc7",marginLeft:"-279px"}}>Country </p>  
+           <input disabled={isView}  className='form-input-field' placeholder='Country'type="text" />
+           <input disabled={isView} className='form-input-field' placeholder='Mobile number' type="number"  />     
+     </form>
+     <Button className='btn save-btn'>Save  <img src={ IconSave } alt="" style={{marginLeft:"80px"}} /></Button>
+     <Button className='btn next-btn' onClick={() => history("/userProfilestore")}>Next <img src={ IconNextArrow } alt=""  style={{marginLeft:"80px"}}/></Button>
+     </div>
+     )} 
+     {workBtn && ( 
+     <div >
+      
+     <form>  
+          <p style={{color:"#702cc7"}}>Address < img alt='' src={ IconAddRound} onClick={addWorkAddressInput} style={{marginLeft:"264px", marginRight:"-4px"}}/> </p>  
+           <input disabled={isView} className='form-input-field' placeholder='Address 1'type="text" />
+           <input disabled={isView} className='form-input-field' placeholder='Address 2' type="text" />
+           <input disabled={isView} className='form-input-field' placeholder='City' type="text" />
+           <input disabled={isView} className='form-input-field' placeholder='District' type="text" />
+           <input disabled={isView} className='form-input-field' placeholder='State' type="text" />
+           <div>
+                {workAddressAddField.map((item, i) => {
                   return (
                     <input
                       // value={item.value}
@@ -145,22 +249,24 @@ function TabForm() {
                   );
                 })}
               </div>
-          </form>
-          <Button className='btn save-btn'>Save  <img src={ IconSave } alt="" style={{marginLeft:"80px"}} /></Button>
-          <Button className='btn next-btn' onClick={handleSelect2}>Next <img src={ IconNextArrow } alt="" style={{marginLeft:"80px"}}/></Button>
-        </div>
-
+     </form>
+     <form>  
+         <p style={{color:"#702cc7",marginLeft:"-279px"}}>Country </p>  
+           <input disabled={isView}  className='form-input-field' placeholder='Country'type="text" />
+           <input disabled={isView}  value="+91" className='form-input-field' placeholder='Mobile number' type="number" />     
+     </form>
+     <Button className='btn save-btn'>Save  <img src={ IconSave } alt="" style={{marginLeft:"80px"}} /></Button>
+     <Button className='btn next-btn' onClick={() => history("/userProfilestore")}>Next <img src={ IconNextArrow } alt="" style={{marginLeft:"80px"}}/></Button>
+     </div>
+     )} 
+          </div>
         </TabPane>
-      
-        <TabPane  onTabClick={callbackTabClicked} tab="Address" key="Address">
-          <AddressTab />
-        </TabPane>
 
 
-      </Tabs>
-    </div> 
+          </Tabs>
+      </div> 
 
-  </div>
+    </div>
   </>
   )
 }
